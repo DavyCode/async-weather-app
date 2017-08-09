6e0bc35a956cee3ccf0f8f8bfdff12450ef8d932
Davy'd <pdaswift3009@gmail.com>
2017-08-09 21:11:46 +0100

    async test

---
+++ async-basics.js
@@ -0,0 +1,12 @@
+console.log("starting the app");
+
+setTimeout( () => {
+     console.log("hey look i finally came..looks like something happened while i was away")
+}, 2000)
+
+setTimeout( () => {
+     console.log("hey look i think im asyncronious")
+}, 0)
+
+
+console.log("app closing")
