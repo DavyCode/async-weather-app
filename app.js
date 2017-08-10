dffbea582f184a682b06cf7abbf574b05f94c6dd
Davy'd <pdaswift3009@gmail.com>
2017-08-10 22:13:38 +0100

    require request

---
+++ app.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+
+request ({
+    url : "https://maps.googleapis.com/maps/api/js?key=AIzaSyB-G2tlet4CPdjtScuygdwO64cUP2jboEc&callback=initMap",
+    json : true
+}, (err, res, body) => {
+    console.log(body);
+});
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
