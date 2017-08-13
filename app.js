2dda614b935d1ac55e1d72de47eccfb145f5db56
Davy'd <pdaswift3009@gmail.com>
2017-08-13 16:47:24 +0100

    require yargs

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const request = require("request");
 
+const yargs = require("yargs");
+
+const argv =  yargs;
+
 
 request ({
     url : "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
@@ -8,7 +12,7 @@ request ({
     console.log("========================= ***************main waether coontent");
     // console.log(JSON.stringify(body.query.results, undefined, 2));
     console.log('Title : ' + body.query.results.channel.title)
-    console.log('City : '+body.query.results.channel.location.city)
+    console.log('City : '+ body.query.results.channel.location.city)
     console.log('Longitude : '+body.query.results.channel.item.long)
     console.log('Latitude : '+body.query.results.channel.item.lat)
     console.log("========================================****************");
